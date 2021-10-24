2922b4a002dd2ea5f8807ec33ef31376c555a136
merdan <merdan.m@gmail.com>
2021-10-24 20:19:08 +0500

    Bulk Demo Category Seeder

---
+++ packages/Sarga/Shop/src/Database/Seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sarga\Shop\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder  extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call(SLocalesTableSeeder::class);
+        $this->call(DemoCategoryTableSeeder::class);
+        $this->call(ChannelTableSeeder::class);
+
+    }
+}
