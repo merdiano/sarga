9fbb512e3f62fdf2d50124644866296965d46188
merdan <merdan.m@gmail.com>
2021-10-24 20:35:38 +0500

    Locales seeder turkmenche ruscha

--- packages/Sarga/Shop/src/Database/Seeders/SLocalesTableSeeder.php
+++ packages/Sarga/Shop/src/Database/Seeders/SLocalesTableSeeder.php
@@ -3,8 +3,21 @@
 namespace Sarga\Shop\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SLocalesTableSeeder extends Seeder
 {
-    public function run(){}
+    public function run(){
+        DB::table('locales')->insert([
+
+            [
+                'id'   => 6,
+                'code' => 'tm',
+                'name' => 'Türkmençe',
+            ], [
+                'id'   => 7,
+                'code' => 'ru',
+                'name' => 'Rusça',
+            ]]);
+    }
 }
