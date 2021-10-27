3b5700ad49cd5d5a0c8aa60024d44c86d38e5516
merdan <merdan.m@gmail.com>
2021-10-27 22:13:21 +0500

    categories tree

---
+++ packages/Sarga/API/Http/Controllers/Categories.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Sarga\API\Http\Controllers;
+
+use Sarga\API\Http\Resources\Catalog\Category;
+use Webkul\API\Http\Controllers\Shop\CategoryController;
+
+class Categories extends CategoryController
+{
+    public function index()
+    {
+        return Category::collection(
+            $this->categoryRepository->getVisibleCategoryTree(request()->input('parent_id'))
+        );
+    }
+
+    public function brands($id)
+    {
+
+    }
+}
