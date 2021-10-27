644d827e83b2e60b3c5fea08d21e68b082608171
merdan <merdan.m@gmail.com>
2021-10-27 22:13:21 +0500

    categories tree

--- packages/Sarga/API/Http/routes.php
+++ packages/Sarga/API/Http/routes.php
@@ -1,10 +1,16 @@
 <?php
+
+use Sarga\API\Http\Controllers\Categories;
 use Sarga\API\Http\Controllers\Channels;
 
 Route::group(['prefix' => 'api'], function ($router) {
     Route::group(['middleware' => ['locale', 'currency']], function ($router) {
         //Channel routes
         Route::get('channels',[Channels::class, 'index']);
-        Route::get('channels/{channel_id}',[Channels::class, 'get']);
+
+        //category routes
+        Route::get('descendant-categories', [Categories::class, 'index']);
+        Route::get('category-brands/{id}', [Categories::class, 'brands']);
+
     });
 });
