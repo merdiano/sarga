51d4d591aaa459a6cb55036598ea81c32b63a6cd
merdan <merdan.m@gmail.com>
2022-04-13 20:30:38 +0500

    vendors

--- packages/Sarga/API/Http/Resources/Catalog/Category.php
+++ packages/Sarga/API/Http/Resources/Catalog/Category.php
@@ -18,24 +18,18 @@ class Category extends JsonResource
         return [
             'id'                 => $this->id,
             'parent_id'          => $this->parent_id,
-//            'code'               => $this->code,
             'name'               => $this->name,
             'slug'               => $this->slug,
-            'trendyol_url'               => $this->trendyol_url,
-            'lcw_url'               => $this->lcw_url,
             'display_mode'       => $this->display_mode,
-//            'description'        => $this->description,
-//            'status'             => $this->status,
             'image_url'          => $this->image_url,
             'category_icon_path' => $this->category_icon_path
                 ? Storage::url($this->category_icon_path)
                 : null,
-//            'additional'         => is_array($this->resource->additional)
-//                ? $this->resource->additional
-//                : json_decode($this->resource->additional, true),
             $this->mergeWhen($this->showChildren(), [
                 'children' => Category::collection($this->children),
                 'product_limit' => $this->product_limit,
+                'trendyol_url'               => $this->trendyol_url,
+                'lcw_url'               => $this->lcw_url,
             ])
         ];
     }
