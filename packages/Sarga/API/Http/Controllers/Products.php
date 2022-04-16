8dcb71b6d804f7517aa19221680eae8f7530323c
merdan <merdan.m@gmail.com>
2022-04-16 15:34:13 +0500

    search suggestions start

--- packages/Sarga/API/Http/Controllers/Products.php
+++ packages/Sarga/API/Http/Controllers/Products.php
@@ -117,7 +117,6 @@ class Products extends ProductController
 
         $key = request('search');
 
-        return $key;
 
         if(!strlen($key)>= 3){
             return response()->json(['message' => '3 karakterden kuchuk','status'=>false]);
