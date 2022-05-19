825559f37c19e0e2c7f3e7588ae2486fc4b2d1f2
merdan <merdan.m@gmail.com>
2022-05-19 16:54:09 +0500

    remove lishniy productstan

--- packages/Sarga/API/Http/Controllers/FilterOptions.php
+++ packages/Sarga/API/Http/Controllers/FilterOptions.php
@@ -53,10 +53,10 @@ class FilterOptions extends \Webkul\RestApi\Http\Controllers\V1\Shop\ResourceCon
             }
 
             if($category = $request->input('category')){
-                $query->join('product_attribute_values',function ($q) use ($request){
+                $query->rightJoin('product_attribute_values',function ($q) use ($request){
                     $q->on('product_attribute_values.integer_value','=','attribute_options.id')
                         ->where('product_attribute_values.attribute_id',$request->get('attribute_id'));
-                })->join('product_categories',function ($q) use($category){
+                })->rightJoin('product_categories',function ($q) use($category){
                     $q->on('product_categories.product_id','=','product_attribute_values.product_id')
                         ->where('product_categories.category_id',$category);
                 });
