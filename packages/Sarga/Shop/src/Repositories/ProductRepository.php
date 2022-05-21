ea7f2d59630c4693ee8be173ae4f0d84b7ae10f6
merdan <merdan.m@gmail.com>
2022-05-21 20:13:14 +0500

    fix log create variant

--- packages/Sarga/Shop/src/Repositories/ProductRepository.php
+++ packages/Sarga/Shop/src/Repositories/ProductRepository.php
@@ -248,7 +248,7 @@ class ProductRepository extends WProductRepository
 
                 # this is key! if a product has been filtered down to the same number of attributes that we filtered on,
                 # we know that it has matched all of the requested filters.
-                $qb->groupBy('variants.id','variants.color');
+                $qb->groupBy('variants.id','variants.size');
                 $qb->havingRaw('COUNT(*) = ' . count($attributeFilters));
             }
 
