c5a818fb3538c48fb1308a9884ee2faacd5bda23
merdan <merdan.m@gmail.com>
2022-05-30 21:56:39 +0500

    management integration start

--- packages/Sarga/Admin/src/Providers/EventServiceProvider.php
+++ packages/Sarga/Admin/src/Providers/EventServiceProvider.php
@@ -11,12 +11,13 @@ namespace Sarga\Admin\Providers;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use Sarga\Admin\Listeners\Customer;
 
 class EventServiceProvider extends ServiceProvider
 {
 
     public function boot(){
-        Event::listen('customer.registration.after', [Sarga\Admin\Listeners\Customer::class,'register']);
+        Event::listen('customer.registration.after', [Customer::class,'register']);
         Event::listen('checkout.order.save.after', 'Webkul\Marketplace\Listeners\Order@afterPlaceOrder');
 //        Event::listen('bagisto.admin.catalog.category.create_form_accordian.general.after', function($viewRenderEventManager) {
 //            $viewRenderEventManager->addTemplate('sarga_admin::catalog.categories.scrap.create');
