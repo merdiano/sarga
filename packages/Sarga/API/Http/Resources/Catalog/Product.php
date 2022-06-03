2959a5344a842a1c1f60a067c7a3eeccb13c4061
merdan <merdan.m@gmail.com>
2022-06-03 12:28:38 +0500

    management integration start

--- packages/Sarga/API/Http/Resources/Catalog/Product.php
+++ packages/Sarga/API/Http/Resources/Catalog/Product.php
@@ -106,7 +106,7 @@ class Product extends JsonResource
         $productTypeInstance = $product->getTypeInstance();
 
         return [
-            'special_price'          => $this->when(
+            'special_price'          => (double) $this->when(
                 $productTypeInstance->haveSpecialPrice(),
                 core()->convertPrice($productTypeInstance->getSpecialPrice())
             ),
@@ -114,7 +114,7 @@ class Product extends JsonResource
                 $productTypeInstance->haveSpecialPrice(),
                 core()->currency($productTypeInstance->getSpecialPrice())
             ),
-            'regular_price'          => $this->when(
+            'regular_price'          => (double) $this->when(
                 $productTypeInstance->haveSpecialPrice(),
                 data_get($productTypeInstance->getProductPrices(), 'regular_price.price')
             ),
