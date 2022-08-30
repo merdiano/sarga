af04ac778296fc53262622ea22155c8ce7d1b8f9
merdan <merdan.m@gmail.com>
2022-08-30 12:21:06 +0500

    wellknow ss

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('app-ads.txt',function(){
     return 'facebook.com, 333307141073441, RESELLER, c3e20eee3f780d68';
 });
+Route::get('/.well-known/pki-validation/CC905B7DBFC9820D09FF9B24B5F8782C.txt',function (){
+    $view = \Illuminate\Support\Facades\Blade::render("{{asset('ssl.txt')}}");
+    return $view;
+});
