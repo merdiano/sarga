da58e9899de58b63b347835eb511e8864d9e90c5
merdan <merdan.m@gmail.com>
2022-09-05 16:23:29 +0500

    flush scout products

--- config/app.php
+++ config/app.php
@@ -289,9 +289,9 @@ return [
         Sarga\Shop\Providers\ShopServiceProvider::class,
         Sarga\API\Providers\APIServiceProvider::class,
         Sarga\Admin\Providers\AdminServiceProvider::class,
-        Sarga\Importer\Providers\ImporterServiceProvider::class,
         Sarga\Brand\Providers\BrandServiceProvider::class,
         Sarga\Payment\Providers\PaymentServiceProvider::class,
+        Sarga\Importer\Providers\ImporterServiceProvider::class,
 //        Webkul\suggestion\Providers\suggestionServiceProvider::class,
 
     ],
