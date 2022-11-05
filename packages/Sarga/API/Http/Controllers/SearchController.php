d886bac3381d60f1c0188b1db41b58288bbb8588
merdan <merdan.m@gmail.com>
2022-11-05 12:05:18 +0500

    invoice fx

--- packages/Sarga/API/Http/Controllers/SearchController.php
+++ packages/Sarga/API/Http/Controllers/SearchController.php
@@ -73,10 +73,10 @@ class SearchController extends V1Controller
         $products = $this->productFlatRepository->getModel()::search(implode(' OR ', $key))
 //            ->where('channel', $channel)
 //            ->where('locale', $locale)
-            ->where('status', 1)
-            ->where('visible_individually', 1)
+//            ->where('status', 1)
+//            ->where('visible_individually', 1)
 //                ->addSelect(DB::raw("\'product\' as type" ))
-            ->orderBy('name')
+//            ->orderBy('name')
             ->take(10)
             ->query(fn ($query) => $query->select('id','name','product_id','description'))
 
