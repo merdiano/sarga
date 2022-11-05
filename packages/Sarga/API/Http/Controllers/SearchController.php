5c103966fae93ccacddeabcac071c86484071768
merdan <merdan.m@gmail.com>
2022-11-05 12:14:49 +0500

    invoice fx

--- packages/Sarga/API/Http/Controllers/SearchController.php
+++ packages/Sarga/API/Http/Controllers/SearchController.php
@@ -78,8 +78,8 @@ class SearchController extends V1Controller
 
             ->take(10)
             ->query(fn ($query) => $query->select('id','name','product_id','description'))
-                ->where('status', 1)
-                ->where('visible_individually', 1)
+//                ->where('status', 1)
+//                ->where('visible_individually', 1)
                 ->orderBy('name')
 //                ->take(10)
             ->get();
