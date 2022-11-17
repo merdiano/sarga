30eea44d48f76fd6da476e9d94933b23dc3c2852
merdan <merdan.m@gmail.com>
2022-11-17 16:08:28 +0500

    discount cart rules

--- packages/Sarga/API/Http/Resources/Checkout/CartRuleResource.php
+++ packages/Sarga/API/Http/Resources/Checkout/CartRuleResource.php
@@ -13,14 +13,16 @@ class CartRuleResource extends JsonResource
 
     public function toArray($request): array
     {
+        $is_fixed = str_contains($this->action_type,'fixed');
         return [
             'id'          => $this->id,
             'name'        => $this->name,
             'description' => $this->description,
-            'action_type' => $this->action_type,
-            'amount'      => str_contains($this->action_type,'fixed')?
-                core()->formatPrice($this->discount_amount,  $this->currency):
-                $this->discount_amount.'%'
+            'is_fixed'    => $is_fixed,
+            'is_discount' => $this->discount_amoun >0,
+            'amount'      => (double) ($is_fixed ?
+                core()->convertPrice($this->discount_amount,  $this->currency):
+                $this->discount_amount)
         ];
     }
 
