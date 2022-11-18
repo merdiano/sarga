0a05ad072b1db133976c435666b74164f9519f33
merdan <merdan.m@gmail.com>
2022-11-18 16:05:36 +0500

    vendor categories fx9

--- packages/Sarga/Admin/src/Http/Controllers/Categories.php
+++ packages/Sarga/Admin/src/Http/Controllers/Categories.php
@@ -2,9 +2,11 @@
 
 namespace Sarga\Admin\Http\Controllers;
 
+use Illuminate\Support\Facades\Event;
 use Sarga\Shop\Repositories\CategoryRepository;
 use Webkul\Attribute\Repositories\AttributeRepository;
 use Webkul\Category\Http\Controllers\CategoryController;
+use Webkul\Category\Http\Requests\CategoryRequest;
 use Webkul\Core\Repositories\ChannelRepository;
 
 class Categories extends CategoryController
@@ -16,5 +18,42 @@ class Categories extends CategoryController
         $this->channelRepository = $channelRepository;
         $this->categoryRepository = $categoryRepository;
         $this->attributeRepository = $attributeRepository;
+    }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Webkul\Category\Http\Requests\CategoryRequest  $categoryRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CategoryRequest $categoryRequest)
+    {
+        Event::dispatch('catalog.category.create.before');
+
+        $category = $this->categoryRepository->create($categoryRequest->all());
+
+//        Event::dispatch('catalog.category.create.after', $category);
+
+        session()->flash('success', trans('admin::app.response.create-success', ['name' => 'Category']));
+
+        return redirect()->route($this->_config['redirect']);
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Webkul\Category\Http\Requests\CategoryRequest  $categoryRequest
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CategoryRequest $categoryRequest, $id)
+    {
+        Event::dispatch('catalog.category.update.before', $id);
+
+        $category = $this->categoryRepository->update($categoryRequest->all(), $id);
+
+//        Event::dispatch('catalog.category.update.after', $category);
+
+        session()->flash('success', trans('admin::app.response.update-success', ['name' => 'Category']));
+
+        return redirect()->route($this->_config['redirect']);
     }
 }
