0abc0c2e6471b25f20410ab628b7cdf4823f27aa
merdan <merdan.m@gmail.com>
2022-11-21 10:05:57 +0500

    notifications ready fx2

--- packages/Sarga/Admin/src/Routes/notification-routes.php
+++ packages/Sarga/Admin/src/Routes/notification-routes.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Sarga\Admin\Http\Controllers\Notifications;
+use Sarga\Admin\Http\Controllers\PushController;
+
 /**
  * Notification routes.
  */
@@ -11,19 +13,19 @@ Route::group(['middleware' => ['web', 'admin'], 'prefix' => config('app.admin_ur
         'redirect' => 'admin.notification.index',
     ])->name('admin.notification.delete-notification');
     //Notifications
-    Route::get('push-notifications', [Notifications::class,'index'])->defaults('_config', [
+    Route::get('push-notifications', [PushController::class,'index'])->defaults('_config', [
         'view' => 'sarga_admin::marketing.notification.index',
     ])->name('admin.push.index');
 
-    Route::get('push-notifications/create', [Notifications::class,'create'])->defaults('_config', [
+    Route::get('push-notifications/create', [PushController::class,'create'])->defaults('_config', [
         'view' => 'sarga_admin::marketing.notification.create',
     ])->name('admin.push.create');
 
-    Route::post('push-notifications/create', [Notifications::class,'store'])->defaults('_config', [
+    Route::post('push-notifications/create', [PushController::class,'store'])->defaults('_config', [
         'redirect' => 'sarga_admin.notifications.index',
     ])->name('admin.push.store');
 
-    Route::get('push-notifications/edit/{id}', [Notifications::class,'edit'])->defaults('_config', [
+    Route::get('push-notifications/edit/{id}', [PushController::class,'edit'])->defaults('_config', [
         'view' => 'sarga_admin::marketing.notification.view',
     ])->name('admin.push.edit');
 });
